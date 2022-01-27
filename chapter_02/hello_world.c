ac355ff7fc8df45c133284ec4be44c1a12ca6a85
Matt Kelly <themattkellyshow@gmail.com>
2022-01-27 04:09:30 -0500

    Completed chapter 2

---
+++ chapter_02/hello_world.c
@@ -0,0 +1,8 @@
+// A "header" file, the standard input/output library
+// Its functions will be available to our program
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  puts("Greetings Humanth!");
+  return 0;
+}
